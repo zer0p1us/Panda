af640bcf18510013df087c5622190d96fa86668a
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-05 17:59:41 +0000

    Added parse function

--- Parser.cs
+++ Parser.cs
@@ -4,6 +4,13 @@ using System.IO;
 
 namespace Parser {
     class Parser{
-        
+        public static List<string> parse(String file_name){
+            List<string> code = new List<string>();
+            //open file and store in code
+
+
+
+            return code;
+        }
     }
 }
