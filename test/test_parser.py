2ccfb7c7441cca7aa4ce116c80926a59404a65aa
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-09 00:53:45 +0000

    Remove unused import module and associated code

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,6 +1,3 @@
-import os
 import subprocess
 
-# os.system("cd ..\\bin\\debug\\netcoreapp3.1\\")
-# os.system("dir")
 subprocess.run(["..\\bin\\debug\\netcoreapp3.1\\Panda.exe", "..\\panda\\print_test.pd"])
