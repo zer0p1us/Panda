ddff3fb01475a3d757f5fb9fddb90a75b4740ed8
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-09 00:44:03 +0000

    Updated the panda script location

--- test/test_parser.py
+++ test/test_parser.py
@@ -3,4 +3,4 @@ import subprocess
 
 # os.system("cd ..\\bin\\debug\\netcoreapp3.1\\")
 # os.system("dir")
-subprocess.run(["..\\bin\\debug\\netcoreapp3.1\\Panda.exe", "..\\bin\\debug\\netcoreapp3.1\\print test.pd"])
+subprocess.run(["..\\bin\\debug\\netcoreapp3.1\\Panda.exe", "..\\panda\\print_test.pd"])
