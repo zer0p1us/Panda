85e32e484e15a1fc3221cc20546b1d75449b97f1
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-09 00:11:18 +0000

    Code lines are now stored in the main file temporarily

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Panda {
-    
+
     class Program {
-    
+
         static void Main(string[] args) {
-            Parser.parse(args[0]);
+            List<string> code_line = new List<string>();
+            Utils.parse(args[0], code_line);
+            Console.Write(code_line[0]);
         }
     }
 }
