7cad227760d2bde873fee37ec534e221ae6be50c
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-09 00:19:40 +0000

    Added python test script

---
+++ test/read_file_test.py
@@ -0,0 +1,6 @@
+import os
+import subprocess
+
+# os.system("cd ..\\bin\\debug\\netcoreapp3.1\\")
+# os.system("dir")
+subprocess.run(["..\\bin\\debug\\netcoreapp3.1\\Panda.exe", "..\\bin\\debug\\netcoreapp3.1\\print test.pd"])
