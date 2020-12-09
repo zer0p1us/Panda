f14fd65a9778baa3e55396e7ca3d0f233d6f334e
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-09 00:55:54 +0000

    Added comment for test script

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,3 +1,5 @@
 import subprocess
 
+# passing panda executable and panda test script
+# panda executable will be generated after build alongside the directory
 subprocess.run(["..\\bin\\debug\\netcoreapp3.1\\Panda.exe", "..\\panda\\print_test.pd"])
