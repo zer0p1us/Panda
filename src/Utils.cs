0949e5f34213aad2c2ffdce0e2ad87a23a55c136
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-10 23:20:16 +0000

    File Stream now part of try catch

--- src/Utils.cs
+++ src/Utils.cs
@@ -7,8 +7,8 @@ namespace Panda {
     class Utils{
         // take in file_name and string list to store line in
         public static void parse(String file_name, List<String> code_line){
-            var code_file = new FileStream( @file_name, FileMode.Open, FileAccess.Read);
             try { // try open file
+                var code_file = new FileStream( @file_name, FileMode.Open, FileAccess.Read);
                 using (var streamReader = new StreamReader(code_file, Encoding.UTF8)){
                     string line; // temporary store of te
                     // check if the end of the file has been reached
@@ -17,7 +17,7 @@ namespace Panda {
                     }
                 }
             } catch(Exception ex){ // if error accours while opening file
-                Console.WriteLine("could not open file");
+                Console.WriteLine("Could not open file");
                 Console.WriteLine(ex.ToString());
             }
         }
