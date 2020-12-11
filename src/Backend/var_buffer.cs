1e9732fce4bfb62fcebab40d3093eae723e36049
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-11 01:28:23 +0000

    Added variable structure

---
+++ src/Backend/var_buffer.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Panda {
+
+    class var_buffer{
+
+        /// <summary>
+        /// variable name is the key and variable value is the value
+        /// </summary>
+        private Dictionary<string, dynamic> P_var = new Dictionary<string, dynamic>();
+
+        /// <summary>
+        /// variable buffer object
+        /// </summary>
+        public var_buffer() {
+        }
+
+        /// <summary>
+        /// create varaible
+        /// </summary>
+        /// <param name="var_name">name of new variable</param>
+        /// <param name="var_value">value of new variable</param>
+        public void set_variable(string var_name, dynamic var_value) {
+            P_var.Add(var_name, var_value);
+        }
+
+        /// <summary>
+        /// get variable value
+        /// </summary>
+        /// <param name="var_name"> the name of the variable </param>
+        /// <returns>dynamic variable value</returns>
+        public dynamic get_variable(string var_name) {
+            dynamic var_value;
+            P_var.TryGetValue(var_name, out var_value);
+            return var_value;
+        }
+    }
+
+}
