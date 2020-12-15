897a31afbc8ba6a15b8ca48ad707bc5a7d9f2cb2
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2020-12-15 13:52:56 +0000

    Added function register

---
+++ src/registry/func_register.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Panda {
+    class func_register {
+        /// <summary>
+        /// store function name and line number as key and value pair in a Dictionary
+        /// </summary>
+        private Dictionary<string, int> P_func = new Dictionary<string, int>();
+
+        /// <summary>
+        /// function register object
+        /// </summary>
+        public func_register() {
+
+        }
+
+        /// <summary>
+        /// create new function
+        /// </summary>
+        /// <param name="func_name">function name</param>
+        /// <param name="func_line">function value</param>
+        public void set_func(string func_name, int func_line) {
+            P_func.Add(func_name, func_line);
+        }
+
+        /// <summary>
+        /// get function line number
+        /// </summary>
+        /// <param name="func_name">function name</param>
+        /// <returns>line number of function</returns>
+        public int get_func(string func_name) {
+            int func_line;
+            P_func.TryGetValue(func_name, out func_line);
+            return func_line;
+        }
+    }
+}
