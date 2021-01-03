238ed073ebc9781df3a4bafb068c0504b466295b
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-01-03 23:55:22 +0000

    Initialised and taking in input correctly

--- src/Program.cs
+++ src/Program.cs
@@ -1,15 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-
-
+using System.Text.RegularExpressions;
 namespace Panda {
 
     class Program {
 
         static void Main(string[] args) {
-            List<string> code_line = new List<string>();
-            Utils.parse(args[0], code_line);
-            Console.Write(code_line[0]);
+            //getting panda source filename
+            string source_filename;
+            if (args.Length == 0 || args == null) {
+                Console.WriteLine("enter Panda source file: ");
+                source_filename = Console.ReadLine();
+            }else { source_filename = args[0]; }
+            
+
+            //initialise list of panda source code
+            List<string> code_list = new List<string>();
+            //initialise variable class
+            Panda.var_register var_Register = new var_register();
+            //initialise function calss
+            Panda.func_register func_Register = new func_register();
+
+            
+            //parse the panda code into the list
+            Utils.parse(source_filename, code_list);
+            
+            for (int i = 0; i < code_list.Count; i++) {
+                lexer.token_collection(code_list[i], var_Register);
+            }
+            Console.Write("done");
+            Console.Read();
+            
         }
     }
 }
