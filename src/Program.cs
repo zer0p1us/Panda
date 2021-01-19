84f07a111f19d59c193e57b990314b14969bfe6d
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-01-19 00:31:56 +0000

    refactored parse function to return array of lines

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 namespace Panda {
-
     class Program {
 
         static void Main(string[] args) {
@@ -12,25 +9,37 @@ namespace Panda {
                 Console.WriteLine("enter Panda source file: ");
                 source_filename = Console.ReadLine();
             }else { source_filename = args[0]; }
-            
+
+            //check langauge option
+            string lang_mode = "en";
+            if (args.Length == 0 || args == null){
+                Console.WriteLine("Executing Panda in english mode");
+                lang_mode = lang_mode + ".lang";
+            } else {lang_mode = args[1] + ".lang"; }
+
 
             //initialise list of panda source code
-            List<string> code_list = new List<string>();
+            //store the content of the panda script file
+            string[] code_list = Utils.parse_file_to_array(source_filename);
+
+            //initialise langauge dependant keywords
+            string[] lang_list = Utils.parse_file_to_array(lang_mode);
+
             //initialise variable class
             Panda.var_register var_Register = new var_register();
-            //initialise function calss
+            //initialise function class
             Panda.func_register func_Register = new func_register();
 
-            
+
             //parse the panda code into the list
-            Utils.parse(source_filename, code_list);
-            
-            for (int i = 0; i < code_list.Count; i++) {
+            //Utils.parse_panda_code(source_filename, code_list);
+
+            for (int i = 0; i < code_list.Length; i++) {
                 lexer.token_collection(code_list[i], var_Register);
             }
             Console.Write("done");
             Console.Read();
-            
+
         }
     }
 }
--- src/Utils.cs
+++ src/Utils.cs
@@ -1,28 +1,32 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 namespace Panda {
     class Utils{
-        
+
         /// <summary>
-        /// read file_name and store in code_line
+        /// read file_name and store in string list
+        /// and return in string array
         /// </summary>
         /// <param name="file_name">name of code file</param>
-        /// <param name="code_line">List to store code lines</param>
-        public static void parse(String file_name, List<String> code_line){
+        public static string[] parse_file_to_array(String file_name){
+            List<string> code_line_list = new List<string>();
             try { // try open file
                 var code_file = new FileStream( @file_name, FileMode.Open, FileAccess.Read);
                 using (var streamReader = new StreamReader(code_file, Encoding.UTF8)){
-                    string line; // temporary store of te
+                    string line; // temporary store of the line
                     // check if the end of the file has been reached
                     while ((line = streamReader.ReadLine()) != null){
-                        code_line.Add(line);
+                        code_line_list.Add(line);
                     }
                 }
-            } catch(FileNotFoundException){ // if error accours while opening file
+                return code_line_list.ToArray();
+            } catch(FileNotFoundException){
+                // if error accours while opening file
                 Console.WriteLine("Could not open file");
+                return null;
             }
         }
     }
