b97ae8cc3d4c747eb97c464d4d0e3038a4e4e7e3
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-01-26 00:56:54 +0000

    added testing of languages

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo BUILDING PANDA
 dotnet build
 echo running panda code
-.\bin\Debug\netcoreapp3.1\Panda.exe .\bin\Debug\netcoreapp3.1\%1
+.\bin\Debug\netcoreapp3.1\Panda.exe .\bin\Debug\netcoreapp3.1\%1 %2
