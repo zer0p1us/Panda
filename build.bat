4dd219308f7576bb61b9e875b9e0fffb412a4f29
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-02-07 19:36:42 +0000

    changed info prompt

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-@echo BUILDING PANDA
+@echo [info] building panda
 dotnet build
-echo running panda code
+@echo [info] running panda code
 .\bin\Debug\netcoreapp3.1\Panda.exe .\bin\Debug\netcoreapp3.1\%1 %2
