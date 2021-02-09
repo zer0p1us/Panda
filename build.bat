5bb4bac04909a3ce2b90f865147daaa2456db0a0
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-02-09 23:43:26 +0000

    fixed path reference

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo [info] building panda
 dotnet build
 @echo [info] running panda code
-.\bin\Debug\netcoreapp3.1\Panda.exe .\bin\Debug\netcoreapp3.1\%1 %2
+.\bin\Debug\netcoreapp3.1\Panda.exe .\panda\%1
