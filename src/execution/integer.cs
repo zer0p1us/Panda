84a70e15e37abf813fb860b079e867a23e87e5bc
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-02-28 19:17:36 +0000

    moved bracket handling to seperate function

--- src/execution/integer.cs
+++ src/execution/integer.cs
@@ -9,18 +9,7 @@ namespace Panda {
             string[] temp_line_array = temp_line.Split(' ');
             //this will replace all the variable tokens with the values of the variables
             if (bracketContent != null){
-                List<string> otherVairableReferences = new List<string>();
-                string tempVariable = "";
-                int startOfVariable = 0;
-                for (int index = 0; index < bracketContent.Length; index++){
-                    if (char.IsLetter(bracketContent[index])){
-                        tempVariable = tempVariable + bracketContent[index];
-                        startOfVariable = index;
-                    } else if (!(char.IsLetter(bracketContent[index])) && tempVariable.Length > 0) {
-                        bracketContent = bracketContent.Replace(tempVariable, int_Register.getVariable(tempVariable).ToString());
-                        tempVariable = "";
-                    }
-                }
+                bracketContent = repaceIntReference(bracketContent, int_Register);
             int_Register.setVariable(temp_line_array[1], EvaluateString.evaluate(bracketContent));
             } else{ /*if the variable stores no*/
                 int_Register.setVariable(temp_line_array[1], EvaluateString.evaluate(temp_line_array[temp_line_array.Length - 1]));
@@ -30,6 +19,22 @@ namespace Panda {
             }else {
                 Console.WriteLine("[info]: integer variable " + temp_line_array[0] + " stores " + int_Register.getVariable(temp_line_array[1]));
             }
+        }
+
+        public static string repaceIntReference(string bracket, int_register int_Register){
+            List<string> otherVairableReferences = new List<string>();
+                string tempVariable = "";
+                int startOfVariable = 0;
+                for (int index = 0; index < bracket.Length; index++){
+                    if (char.IsLetter(bracket[index])){
+                        tempVariable = tempVariable + bracket[index];
+                        startOfVariable = index;
+                    } else if (!(char.IsLetter(bracket[index])) && tempVariable.Length > 0) {
+                        bracket = bracket.Replace(tempVariable, int_Register.getVariable(tempVariable).ToString());
+                        tempVariable = "";
+                    }
+                }
+            return bracket;
         }
     }
 
