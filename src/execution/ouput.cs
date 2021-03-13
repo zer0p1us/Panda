a04a3b8a196e0d9d1ee7ed31236cf5876d7ceb64
zer0p1us <42619260+zer0p1us@users.noreply.github.com>
2021-03-13 01:56:38 +0000

    added output class

---
+++ src/execution/ouput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.execution {
+    class ouput {
+        
+        public static void _output(string rawOutputData) {
+            string processed = integer.repaceIntReference(extractBracketContent(rawOutputData));
+            processed.Replace("(", "");
+            processed.Replace(")", "");
+            Console.WriteLine(processed);
+        }
+
+        private static string extractBracketContent(string bracketWrappedExpression) {
+            //check if it contains brackets
+            //if so extract the content of the brackets without the brackets
+            if (bracketWrappedExpression.Contains('(')) {
+                return bracketWrappedExpression.Substring(bracketWrappedExpression.IndexOf('('));
+            }
+            else if (bracketWrappedExpression.Contains('[')) {
+                return bracketWrappedExpression.Substring(bracketWrappedExpression.IndexOf('['));
+            }
+            else if (bracketWrappedExpression.Contains('{')) {
+                return bracketWrappedExpression.Substring(bracketWrappedExpression.IndexOf('{'));
+            }
+            else {
+                return bracketWrappedExpression;
+            }
+        }
+    }
+}
